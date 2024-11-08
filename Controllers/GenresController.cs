e61199918920056caf417991c0507f3c25d54cb9
Conte <brunofelipeconte@gmail.com>
2024-11-08 16:24:25 -0300

    Projeto atualizado até a quinta parte do roteiro - Jovem programador

---
+++ Controllers/GenresController.cs
@@ -0,0 +1,23 @@
+﻿using Bookstore2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Bookstore2.Controllers
+{
+    public class GenresController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Genre> genres = new List<Genre>
+            {
+                new Genre(1,"Drama"),
+                new Genre(2,"Comédia"),
+                new Genre(3,"Terror"),
+                new Genre(4,"Ação"),
+                new Genre(5,"Fantasia")
+            };
+            return View(genres);
+
+        }
+    }
+}
---
+++ Data/BookstoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Bookstore2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookstore2.Data
+{
+    public class BookstoreContext : DbContext
+    {
+        public BookstoreContext(DbContextOptions<BookstoreContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
